65c87993655b0c1b4c99404ae3f111dfeec15792
Nikita <valuev_n@me.com>
2019-08-23 18:49:49 +0300

    Добавил длину income и Вывод периода с целью

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ let income = '15';
 let addExpenses = '4, 8, 2';
 let deposit = true;
 let mission = 21000;
-let iod = 12;
+let period = 12;
 
 //alert('Need more money!');
 //console.log('They took my candle');
 
 console.log(typeof (money) + '\n', typeof (income) + '\n', typeof (deposit) + '\n');
+console.log(income.length);
+console.log('Период ' + period + ' месяцев.'+'\n\nЦель заработать ' + mission + ' рублей');
