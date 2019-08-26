212f2955368b04df15e3d69360ee0d7f6ce01992
Nikita <valuev_n@me.com>
2019-08-26 10:30:40 +0300

    добавил задание с addExpanses

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let money = 25;
 let income = '15';
-let addExpenses = '4, 8, 2';
+let addExpenses = 'Аренда, Реклама, Транспорт';
 let deposit = true;
 let mission = 21000;
 let period = 12;
@@ -8,9 +8,12 @@ let budgetDay = money / 30;
 
 //alert('Need more money!');
 //console.log('They took my candle');
+let addExpensesLower = addExpenses.toLowerCase()
+
 
 console.log(typeof (money) + '\n', typeof (income) + '\n', typeof (deposit) + '\n');
 console.log(income.length);
 console.log('Период ' + period + ' месяцев.'+'\n\nЦель заработать ' + mission + ' рублей');
-console.log(budgetDay + 'Остаток от деления ' + (money % 30));
+console.log(addExpensesLower.split(','));
+console.log(budgetDay + ' Остаток от деления ' + (money % 30));
 
